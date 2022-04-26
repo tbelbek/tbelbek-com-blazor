a634a7972b0bcfb15e845bbd6415d0e073a3a323
Tughan Belbek <tughan.belbek@dowhile.se>
2022-04-26 09:21:34 +0200

    dummy data for build

--- tbelbek.com/Startup.cs
+++ tbelbek.com/Startup.cs
@@ -29,6 +29,7 @@ namespace tbelbek.com
             services.AddSingleton<IStringLocalizer, StringLocalizer<Resource>>();
             services.AddScoped<IDataStore, DataStore>();
             services.AddScoped<ISiteDataParser, SiteDataParser>();
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
